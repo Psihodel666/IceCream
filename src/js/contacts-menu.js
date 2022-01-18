5edb245e9c5019d4682a08ace90a2926dd9eb34f
Artem-Urban <urban9316@gmail.com>
2022-01-18 15:25:49 +0200

    add contacts table and desctop #2

--- src/js/contacts-menu.js
+++ src/js/contacts-menu.js
@@ -1,14 +1,14 @@
+
 (() => {
     const refs = {
         openContactsMenu: document.querySelector('[open-contacts-menu]'),
         closeContactsMenu: document.querySelector('[close-contacts-menu]'),
         menu: document.querySelector('[contacts-menu]'),
     };
-
     refs.openContactsMenu.addEventListener('click', toggleMenu);
     refs.closeContactsMenu.addEventListener('click', toggleMenu);
-
     function toggleMenu() {
+        document.body.classList.toggle("modal-open");
         refs.menu.classList.toggle('is-hidden');
     }
 })();
